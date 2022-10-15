7fce452d5b30310bc156bcb1be453cb18d30b291
ikra01 <ikramajid1@gmail.com>
2022-10-15 14:13:33 +0100

    Ikra's update - connected to mongodb & removed handlebars

---
+++ express/database/conn.js
@@ -0,0 +1,12 @@
+const mongoose=require('mongoose');
+mongoose.connect("mongodb://127.0.0.1:27017/hr-management-system",{
+    useNewUrlParser:true,
+    useUnifiedTopology:true  
+}).then(()=>
+{
+    console.log("Connection to database successful")
+}).catch((err)=>
+{
+    console.log('Connection to database unsucessful');
+    console.log(err);
+})
---
+++ frontend/src/Components/Departments.js
@@ -0,0 +1,126 @@
+import './Departments.css'
+
+function departments () {
+    return (
+        <div class="page-content">
+    
+
+    <div class="dBoxes">
+        <h2>Events</h2>
+        <div class="roles-box">
+            <p class="role-weight" >Director of Facilities</p>
+            <p>Employees: 1</p>
+        </div>
+        <div class="roles-box">
+            <p class="role-weight">Events Manager</p>
+            <p>Employees: 1</p>
+        </div>
+        <div class="roles-box">
+            <p class="role-weight">Events Coordinator</p>
+            <p>Employees: 2</p>
+        </div>
+    </div>
+    <div class="dBoxes">
+        <h2>HR</h2>
+        <div class="roles-box">
+            <p class="role-weight">Director of Operations and People</p>
+            <p>Employees: 1</p>
+        </div>
+        <div class="roles-box">
+            <p class="role-weight">HR Manager</p>
+            <p>Employees: 1</p>
+        </div>
+    </div>
+    <div class="dBoxes"> 
+        <h2>Innovation</h2>
+        <div class="roles-box">
+            <p class="role-weight">Director of Programmes</p>
+            <p>Employees: 1</p>
+        </div>
+        <div class="roles-box">
+            <p class="role-weight">Innovation Community Manager</p>
+            <p>Employees: 1</p>
+        </div>
+        <div class="roles-box">
+            <p class="role-weight">Innovation Techincal Manager</p>
+            <p>Employees: 1</p>
+        </div>
+        <div class="roles-box">
+            <p class="role-weight">Senior Programmes Manager</p>
+            <p>Employees: 1</p>
+        </div>
+        <div class="roles-box">
+            <p class="role-weight">Senior Programmes Coordinator</p>
+            <p>Employees: 2</p>
+        </div>
+    </div>
+    <div class="dBoxes">
+        <h2>Marketing</h2>
+        <div class="roles-box">
+            <p class="role-weight">Director of Marketing</p>
+            <p>Employees: 1</p>
+        </div>
+        <div class="roles-box">
+            <p class="role-weight">Marketing Manager</p>
+            <p>Employees: 3</p>
+        </div>
+        <div class="roles-box">
+            <p class="role-weight">Marketing Associate</p>
+            <p>Employees: 5</p>
+        </div>     
+    </div>
+    <div class="dBoxes">
+        <h2>Sales</h2>
+        <div class="roles-box">
+            <p class="role-weight">Sales Manager</p>
+            <p>Employees: 1</p>
+        </div>
+        <div class="roles-box">
+            <p class="role-weight">Sales Manager - Programmes</p>
+            <p>Employees: 1</p>
+        </div>
+        <div class="roles-box">
+            <p class="role-weight">Client Relationship Manager</p>
+            <p>Employees: 1</p>
+        </div>
+        <div class="roles-box">
+            <p class="role-weight">Sales Executive</p>
+            <p>Employees: 3</p>
+        </div>
+    </div>
+
+
+    <div class="dBoxes">
+        <h2>Skills City</h2> 
+        <div class="roles-box">
+            <p class="role-weight">Director of Education</p>
+            <p>Employees: 1</p>
+        </div>
+        <div class="roles-box">
+        <p class="role-weight">Training Manager</p>
+        <p>Employees: 1</p>
+        </div>
+        <div class="roles-box">
+        <p class="role-weight">Digital Technician</p>
+        <p>Employees: 6</p>
+        </div>
+        <div class="roles-box">
+        <p class="role-weight">Data Administration</p>
+        <p>Employees: 1</p>
+        </div>
+        <div class="roles-box">
+        <p class="role-weight">Delivery Manager</p>
+        <p>Employees: 1</p>
+        </div>
+    </div>
+
+
+    
+</div>
+
+
+
+    )
+}
+
+export default departments;
